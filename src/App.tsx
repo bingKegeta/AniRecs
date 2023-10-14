38763fd4fbdadb9b9db9bbff96b39f1dcab2aca3
Soham Ganguly <sohamg24@gmail.com>
2023-10-14 16:19:31 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import SearchBar from './SearchBar'
+
+function App() {
+
+  return (
+    <>
+      <div className='search-bar-container'>
+        <SearchBar></SearchBar>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/SearchBar.tsx
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+
+const SearchBar = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [isExpanded, setIsExpanded] = useState(false);
+  const [isSearched, setIsSearched] = useState(false);
+
+  const handleSearch = () => {
+    if (searchTerm.trim() !== '') {
+        console.log(searchTerm, "searched");
+    }
+    setIsSearched(true);
+    // TODO: Display the cards here or somewhere else not sure
+  };
+
+  const handleKeyPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') {
+      handleSearch();
+    }
+  };
+
+  const onFocus = () => {
+      setIsExpanded(true);
+  };
+
+  const onBlur = () => {
+    setIsExpanded(false);
+  }
+
+
+  return (
+    <div className='full-bar'>
+        <div className={`search-bar ${isExpanded ? 'expanded' : isSearched ? 'searched' : ''}`}>
+            <div className="search-input" onFocus={onFocus} onBlur={onBlur}>
+                <input
+                    type="text"
+                    placeholder="Search..."
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)}
+                    onKeyDown={handleKeyPress}
+                />
+                <button onClick={handleSearch}>Go</button>
+            </div>
+        </div>
+    </div>
+  );
+};
+
+export default SearchBar;
