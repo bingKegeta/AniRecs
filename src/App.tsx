7a83090708def19fd859d0f30d895ddace6cad90
Soham Ganguly <sohamg24@gmail.com>
2023-10-17 19:18:04 -0400

    Set tests

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,15 @@ import SearchBar from './components/SearchBar'
 
 function App() {
 
+  const onSearch = (query: string) => {
+    alert("You searched " + query + "!");
+  }
+
   return (
     <>
       <div className='search-bar-container'>
         <Header text='Enter your anilist username to get a list of recommendations for your next anime!' size={2}></Header>
-        <SearchBar></SearchBar>
+        <SearchBar text='Enter Anilist Username...' onSearch={onSearch}></SearchBar>
       </div>
     </>
   );
