b29636d368db7901a0850042f5155dd1e4cbe56c
Soham Ganguly <sohamg24@gmail.com>
2023-10-17 20:18:08 -0400

    Add username check to backend

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,23 @@ import SearchBar from './components/SearchBar'
 function App() {
 
   const onSearch = (query: string) => {
-    alert("You searched " + query + "!");
+    
+    const searchData = { username:query};
+
+    fetch('http://localhost:5000/check_user', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(searchData),
+    })
+    .then(response => response.json())
+    .then(data => {
+      console.log('Search result:', data);
+    })
+    .catch(error => {
+      console.log("Error: ", error)
+    });
   }
 
   return (
